a390ec69e270020ecb912973f7b5af0e41fbfe0d
b07901135 <b07901135@g.ntu.edu.tw>
2019-07-24 00:35:47 +0800

    fuck me again

--- src_rnn/generate_input.R
+++ src_rnn/generate_input.R
@@ -3,21 +3,24 @@ library(dplyr)
 library(Matrix)
 setwd('~/Documents/2019dsp-summer-project')
 
-path_raw <- './data_wtv/scrambled.csv'
-path_table <- './data_wtv/word_vectors.csv'
+path_token_list <- './data_wtv/token_list'
+path_label_list <- './data_wtv/label_list'
+path_table <- './data_wtv/w2v_table.dm'
 path_output <- './data_rnn/'
-fix_length <- 40
+fix_length <- 30
 
-N <- 1000
+N <- 20000
 
-raw <- read.csv( path_raw, header=T, skip=20000, nrows=N )
-w2v_table <- read.csv( path_table, header=T )
+load(path_token_list)
+load(path_label_list)
+load(path_table)
+tokenList <- tokenList[1:N]
+labelList <- labelList[1:N]
 
-
-width <- ncol(w2v_table)
-length <- nrow(w2v_table)
-rownames( w2v_table ) <- w2v_table[,1]
-w2v_table <- select( w2v_table, -X )
+#width <- ncol(w2v_table)
+#length <- nrow(w2v_table)
+#rownames( w2v_table ) <- w2v_table[,1]
+#w2v_table <- select( w2v_table, -X )
 
 ws2v <- function(w) { 
     v <- w2v_table[w,] 
@@ -33,15 +36,15 @@ extend <- function( d ) {
         return( rbind( d, matrix(0, fix_length-len, 200 ) ) )
     }
 }
-wv <- tokenize_tweets( as.character(raw[,3]), lowercase=T, strip_punct=F, simplify=F ) %>% 
+wv <- tokenize_tweets( as.character(tokenList), lowercase=T, strip_punct=F, simplify=F ) %>% 
     lapply( ws2v ) %>% 
     lapply( extend ) %>% 
     lapply( as.matrix ) %>% 
     lapply( t ) %>%
     unlist( recursive=F ) %>%  
     as.array()
-dim( wv ) <- c( N, 40, 200 )
+dim( wv ) <- c( N, fix_length, 200 )
 save( wv, file=paste0(path_output, 'vector_test') )
 
-labels <- as.numeric( raw[,2] == 4 ) 
+labels <- as.numeric(  labelList==4 ) 
 save( labels, file=paste0(path_output, 'label_test') )
