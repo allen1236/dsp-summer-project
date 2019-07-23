78c20e6cf4370238cb5e5f3212d98b65353b4059
b07901135 <b07901135@g.ntu.edu.tw>
2019-07-23 19:48:21 +0800

    start traning

--- src_rnn/rnn_1.R
+++ src_rnn/rnn_1.R
@@ -1,32 +1,42 @@
 library(tensorflow)
 library(keras)
 library(dplyr)
+setwd('~/Documents/2019dsp-summer-project/')
 
 install_tensorflow( version='gpu' )
 install_keras( tensorflow='gpu' )
 
-N <- 500
-l <- 20
-w <- 20
+N <- 10000
+l <- 40
+w <- 200
 
 set.seed(123)
+str <- "
 n <-  seq(1:N)
 x <-  rnorm(N*l*w)
 dim(x) <- c(N,l,w)
 y <- rep( c(0,1,0), N )
 dim(y) <- c(N,3)
+"
+
+wv <- wv %>%  as.matrix()
+
 
 model <- keras_model_sequential() %>% 
-  layer_lstm( units=128, input_shape=c(l, w), activation='sigmoid' ) %>% 
-  layer_dense( units=64, activation='sigmoid' ) %>% 
-  layer_dense( units=32, activation='sigmoid' ) %>% 
+  layer_lstm( units=512, input_shape=c(l, w), activation='sigmoid' ) %>% 
+  layer_dense( units=256, activation='sigmoid' ) %>% 
+  layer_dense( units=128, activation='sigmoid' ) %>% 
   layer_dense( units=3, activation='softmax' )
 model %>% compile( loss = 'categorical_crossentropy',
                   optimizer = 'adam',
                   metrics = list("accuracy")
 )
+
+load( 'data_rnn/label' )
+load( 'data_rnn/vector' ) 
+
 model %>% summary()
-model %>% fit( x, y, epochs=30, batch_size=32, shuffle=FALSE )
-y_pred <- model %>% predict( x )
-scores <-  model %>% evaluate( x, y, verbose=0 )
+model %>% fit( wv, labels, epochs=20, batch_size=32, shuffle=FALSE )
+y_pred <- model %>% predict( wv )
+scores <-  model %>% evaluate( wv, labels, verbose=0 )
 print( scores )
